517a75e8ddf6bb9c5267cd868a280d052aae133c
Nav-03 <navil.gabrie@gmail.com>
2022-01-06 04:30:14 +0000

    set up home and secondscounter

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SecondsCounter = () => {
+	return (
+		<div className="bigCounter">
+			<div className="clockIcon">
+				<i class="far fa-clock"></i>
+			</div>
+			<div className="six">0</div>
+			<div className="five">0</div>
+			<div className="four">0</div>
+			<div className="three">0</div>
+			<div className="two">0</div>
+			<div className="one">0</div>
+		</div>
+	);
+};
+
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,16 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import SecondsCounter from "./component/SecondsCounter.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const startTime = Date.now();
+
+setInterval(() => {
+	let timediff = Date.now() - startTime;
+	let increase = Math.floor(timediff / 1000);
+	ReactDOM.render(
+		<SecondsCounter seconds={increase} />,
+		document.querySelector("#app")
+	);
+}, 1000);
