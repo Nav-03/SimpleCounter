ec97e6fab156976707c4a80f269980cab4bd355e
Nav-03 <navil.gabrie@gmail.com>
2022-01-06 13:11:41 +0000

    final before extra credit

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,52 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import SecondsCounter from "./component/SecondsCounter.jsx";
 
-
-
-function(props) {
+function SimpleCounter(props) {
 	return (
 		<div className="bigCounter">
 			<div className="calendar">
 				<i class="far fa-clock"></i>
 			</div>
-			<div className="four">{props.digitFour}</div>
-			<div className="three">{props.digitThree}</div>
-			<div className="two">{props.digitTow}</div>
-			<div className="one">{props.digitOne}</div>
-
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
 		</div>
-	)
+	);
 }
 
+SimpleCounter.prototypes = {
+	digitFour: propTypes.number,
+	digitThree: propTypes.number,
+	digitTwo: propTypes.number,
+	digitOne: propTypes.number,
+};
+
 let counter = 0;
-setInterval(funtion(){
+setInterval(function () {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+	counter++;
+
 	ReactDOM.render(
-		<SecondsCounter />,
-		document.querySelector('#app')
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
 	);
 }, 1000);
-
 
 //render your react application
